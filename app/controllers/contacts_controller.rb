9f1cf74ef102688def2b7454e6f841c172864b1e
squareleaf <tiffany.green@wellbe.me>
2017-09-12 20:34:59 -0500

    Remember sort params

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,11 @@ class ContactsController < ApplicationController
   helper_method :column_sort, :direction_sort
 
   def index
+    if params[:sort] || params[:direction]
+      session[:column_sort] = params[:sort]
+      session[:direction_sort] = params[:direction]
+    end
+
     @contacts = Contact.order(column_sort + " " + direction_sort)
   end
 
@@ -23,10 +28,12 @@ class ContactsController < ApplicationController
   private
 
   def column_sort
+    return session[:column_sort] if session[:column_sort]
     Contact.column_names.include?(params[:sort]) ? params[:sort] : "name"
   end
 
   def direction_sort
+    return session[:direction_sort] if session[:direction_sort]
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
