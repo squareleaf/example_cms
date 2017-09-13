5488335599855f271cf3cef03e7ba9232dc76d27
squareleaf <tiffany.green@wellbe.me>
2017-09-12 21:33:47 -0500

    Add avatars using gravatar and style

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,9 @@ class Contact < ApplicationRecord
   validates :email, uniqueness: {scope: :name}, allow_blank: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates_format_of :phone, with: /(\d{3}-)?\d{3}-\d{4}/
+
+  def avatar
+    clean_email = email.strip.downcase
+    Digest::MD5.hexdigest(clean_email)
+  end
 end
